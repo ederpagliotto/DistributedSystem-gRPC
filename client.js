29380022d97ea36b5c3e396ae6aec614b1342a92
Eder Pagliotto <ederpagliotto@gmail.com>
2024-04-19 17:43:58 +0100

    starting to create function to select choice

--- client.js
+++ client.js
@@ -15,3 +15,30 @@ const client = new smartRetailProto.SmartRetail(
   'localhost:50051',
   grpc.credentials.createInsecure(),
 );
+
+function askQuestion(query) {
+  return new Promise((resolve) => rl.question(query, resolve));
+}
+
+let baggedItems = [];
+
+async function main() {
+  console.log('Welcome to Smart Retail Checkout!');
+  await askForChoice();
+}
+
+async function askForChoice() {
+  console.log('\nSelect an option:');
+  console.log('1: List Products');
+  console.log('2: Scan Item');
+  console.log('3: Bag Item');
+  console.log('4: Track Bagged Items');
+  console.log('5: Checkout');
+  console.log('6: Exit');
+
+  const choice = await askQuestion('Enter choice: ');
+  console.log('*******************************************');
+  processChoice(choice.trim());
+}
+
+main();
